e37db594c0956b88b43b9cc7bb03a8a35dc824e2
stepan.datsko <stepan.datsko@ringcentral.com>
2026-06-27 21:55:24 +0300

    initial

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+﻿#include <chrono>
+#include <iostream>
+#include <string_view>
+#include <thread>
+
+int main() {
+    constexpr std::string_view message = "Hello from C++20, CMake, and Clang!";
+    std::cout << message << '\n';
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+    return 0;
+}
---
+++ tools/cmake_with_vsdev.cmd
@@ -0,0 +1,6 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=x64 -host_arch=x64
+if errorlevel 1 exit /b %errorlevel%
+
+cmake %*
+exit /b %errorlevel%
